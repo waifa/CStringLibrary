21c346a28ed6559114972e11d89abde18c9c1eff
Ubuntu <ubuntu@ip-172-31-12-179.us-east-2.compute.internal>
2021-05-17 16:31:48 +0000

    completed first function, need to fix how it is returned

--- PythonString.c
+++ PythonString.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 void capitalize(char* string){
-    string[0] = "A";
-    return string;
+    string[0] = 'A';
+    printf("%s", string);
 }
 
 int main(int argc, char *argv[]) {
     printf("%s", "Hey, welcome to my C library\n");
+	char test[] = "apple";
+	capitalize(test);
+	printf("\n");
     return 0;
 }
