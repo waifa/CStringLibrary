3404cf1b56983f5c5450ad16f524765e747943e8
Muhammad Huzaifa <huzaifam2015@gmail.com>
2021-05-17 12:27:38 -0400

    s

--- PythonString.c
+++ PythonString.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+void capitalize(char* string){
+    string[0] = "A";
+    return string;
+}
+
 int main(int argc, char *argv[]) {
     printf("%s", "Hey, welcome to my C library\n");
     return 0;
